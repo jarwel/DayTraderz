c60f41ff8300d7635868dec87fb21e44eaccef7a
jwells <jwells@yahoo-inc.com>
2015-01-09 12:09:58 -0800

    initial commit

---
+++ DayTraderz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayTraderz
+//
+//  Created by Jason Wells on 1/9/15.
+//  Copyright (c) 2015 Jason Wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
