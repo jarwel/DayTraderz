da3f4171772d1e7124f0a940da2164eb821f41ee
Jason Wells <jarwel86@gmail.com>
2015-07-09 09:52:21 -0700

    refactored AccountCell to swift

---
+++ DayTrades/AccountCell.swift
@@ -0,0 +1,27 @@
+//
+//  AccountCell.swift
+//  DayTrades
+//
+//  Created by Jason Wells on 7/9/15.
+//  Copyright (c) 2015 Jason Wells. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AccountCell : UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var winnersLabel: UILabel!
+    @IBOutlet weak var losersLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    func clear() {
+        self.nameLabel.text = nil
+        self.winnersLabel.text = nil
+        self.losersLabel.text = nil
+        self.valueLabel.text = nil
+    }
+    
+}
+
--- DayTrades/LeadersViewController.m
+++ DayTrades/LeadersViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "LeadersViewController.h"
+#import "DayTrades-Swift.h"
 #import "UIScrollView+SVInfiniteScrolling.h"
 #import "Account.h"
-#import "AccountCell.h"
 #import "ParseClient.h"
 #import "PriceFormatter.h"
 #import "DateHelper.h"
@@ -61,7 +61,7 @@ static NSString * const cellIdentifier = @"AccountCell";
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     AccountCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
-    [cell clearFields];
+    [cell clear];
     
     if (indexPath.section == 0) {
         Account *account = [self.accounts objectAtIndex:indexPath.row];
